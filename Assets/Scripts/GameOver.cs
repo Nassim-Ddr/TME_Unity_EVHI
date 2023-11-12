5d5e7e451beb074a9f9fab291a1100bc653b1f1d
White-On <74248238+White-On@users.noreply.github.com>
2023-11-12 19:18:19 +0100

    adding GameOver manager

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+	public GameObject gameOverUI;
+	public Text ScoreUI;
+	public Scene scene;
+	bool gameOver;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		FindObjectOfType<PlayerController>().OnPlayerDeath += OnGameOver;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (gameOver)
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				SceneManager.LoadScene(0);
+			}
+		}
+	}
+
+	void OnGameOver()
+	{
+		gameOverUI.SetActive(true);
+		ScoreUI.text = Mathf.RoundToInt(Time.timeSinceLevelLoad).ToString();
+		gameOver = true;
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,23 @@ public class PlayerController : MonoBehaviour
     public GameSettings gameSettings;
     public Rigidbody2D rb;
 
-    private float moveX;
+	float screenHalfWidthInWorldUnits;
+	Vector2 screenHalfSizeInWorldUnits;
+
+	public event System.Action OnPlayerDeath;
+
+	private float moveX;
     // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         speed = gameSettings.playerSpeed;
-    }
+
+		// calculate the screen bounds
+		float halfPlayerWidth = transform.localScale.x / 2f; // half the player width
+		screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize + halfPlayerWidth;
+		screenHalfSizeInWorldUnits = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
+	}
 
     // Update is called once per frame
     void Update()
@@ -25,5 +35,38 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
 	{
 		rb.velocity = new Vector2(moveX, rb.velocity.y);
+
+		if (transform.position.x < -screenHalfWidthInWorldUnits)
+		{
+			transform.position = new Vector2(screenHalfWidthInWorldUnits, transform.position.y);
+		}
+
+		if (transform.position.x > screenHalfWidthInWorldUnits)
+		{
+			transform.position = new Vector2(-screenHalfWidthInWorldUnits, transform.position.y);
+		}
+
+		// if the player goes out of bounds, kill the player
+		if (transform.position.y < -screenHalfSizeInWorldUnits.y)
+		{
+			PlayerDie();
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D triggerCollider)
+	{
+		if (triggerCollider.tag == "Enemies")
+		{
+			PlayerDie();
+		}
+	}
+
+	void PlayerDie(){
+		if (OnPlayerDeath != null)
+		{
+			OnPlayerDeath();
+		}
+		Destroy(gameObject);
 	}
 }
