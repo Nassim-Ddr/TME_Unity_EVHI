0ef2e73cb8d674ee9a2a2737ef818d7e7e037b99
White-On <74248238+White-On@users.noreply.github.com>
2023-11-13 11:28:24 +0100

    Spawning random enemies

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -10,24 +10,52 @@ public class LevelGenerator : MonoBehaviour
     public float probaPlatformMobile = 0.2f;
     public GameObject platformMobilePrefab; 
     
-    public float minDistanceY = .5f;
-    public float maxDistanceY = 1.5f;
+    public float PlatformMinDistanceY = .5f;
+    public float PlateformMaxDistanceY = 1.5f;
 
-    public float maxDistanceX = 2f;
+    public float PlatformMaxDistanceX = 2f;
 
-    void Start()
+    public int number_enemies = 100;
+	public float EnemyMaxDistanceFromMiddle = 2f;
+	public float EnemyMinDistanceY = 5f;
+	public float EnemyMaxDistanceY = 15f;
+	// list prefab enemy
+	public List<GameObject> enemyPrefabList = new List<GameObject>();
+    
+
+	void Start()
     {
-        Vector3 spawnPosition = new Vector3(); 
-        
+		GenerateAllPlatform();
+		GenerateAllEnemies();
+	}
 
-        for (int i = 0; i < 500; i++)
-        {
-            spawnPosition.y += Random.Range(minDistanceY, maxDistanceY);
-            spawnPosition.x = Random.Range(-maxDistanceX, maxDistanceX);
+    void GenerateAllPlatform()
+	{
+		Vector3 spawnPosition = new Vector3();
 
-            float proba = Random.Range(0.0f, 1.0f);
-            if (proba <= probaPlatformMobile) Instantiate(platformMobilePrefab, spawnPosition, Quaternion.identity);
-            else Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
-        }
-    }
+		// Platform generation
+		for (int i = 0; i < 500; i++)
+		{
+			spawnPosition.y += Random.Range(PlatformMinDistanceY, PlateformMaxDistanceY);
+			spawnPosition.x = Random.Range(-PlatformMaxDistanceX, PlatformMaxDistanceX);
+
+			float proba = Random.Range(0.0f, 1.0f);
+			if (proba <= probaPlatformMobile) Instantiate(platformMobilePrefab, spawnPosition, Quaternion.identity);
+			else Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+		}
+	}
+
+	void GenerateAllEnemies(){
+		Vector3 spawnPosition = new Vector3();
+		// Enemy generation
+		for (int i = 0; i < number_enemies; i++)
+		{
+			spawnPosition.y += Random.Range(EnemyMinDistanceY, EnemyMaxDistanceY);
+			spawnPosition.x = Random.Range(-EnemyMaxDistanceFromMiddle, EnemyMaxDistanceFromMiddle);
+
+			// Instantiate a random enemy prefab from the list
+			GameObject enemyPrefab = enemyPrefabList[Random.Range(0, enemyPrefabList.Count)];
+			Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+		}
+	}
 }
