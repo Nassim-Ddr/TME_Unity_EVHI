488e37d47227fdf74f7147fb7051e776dd9e489e
White-On <74248238+White-On@users.noreply.github.com>
2023-11-13 14:45:59 +0100

    add decoration

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -21,12 +21,17 @@ public class LevelGenerator : MonoBehaviour
 	public float EnemyMaxDistanceY = 15f;
 	// list prefab enemy
 	public List<GameObject> enemyPrefabList = new List<GameObject>();
+	// list prefab decoration
+	public List<GameObject> decorationPrefabList = new List<GameObject>();
+	public int number_decoration = 100;
+	public float DecorationMinDistanceY = 5f;
+	public float DecorationMaxDistanceY = 15f;
     
-
 	void Start()
     {
 		GenerateAllPlatform();
 		GenerateAllEnemies();
+		GenerateAllDecoration();
 	}
 
     void GenerateAllPlatform()
@@ -56,6 +61,42 @@ public class LevelGenerator : MonoBehaviour
 			// Instantiate a random enemy prefab from the list
 			GameObject enemyPrefab = enemyPrefabList[Random.Range(0, enemyPrefabList.Count)];
 			Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+		}
+	}
+
+	private void GenerateAllDecoration()
+	{
+		Vector3 spawnPosition = new Vector3();
+		Vector2 screenHalfSizeInWorldUnits = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
+		
+		float RightBorder = screenHalfSizeInWorldUnits.x;
+		float LeftBorder = -screenHalfSizeInWorldUnits.x;
+		List<float> values = new List<float>();
+		values.Add(RightBorder);
+		values.Add(LeftBorder);
+		int leafOrRight;
+		// Enemy generation
+		for (int i = 0; i < number_decoration; i++)
+		{
+			leafOrRight = Random.Range(0, values.Count);
+			spawnPosition.y += Random.Range(EnemyMinDistanceY, EnemyMaxDistanceY);
+			// choose random side
+			spawnPosition.x = values[leafOrRight];
+			// Intantiate a random enemy prefab from the list
+			GameObject decorationPrefab = decorationPrefabList[Random.Range(0, decorationPrefabList.Count)];
+			GameObject decoration = Instantiate(decorationPrefab, spawnPosition, Quaternion.identity);
+			// flip the decoration if it is on the left side
+			if (leafOrRight == 1)
+			{
+				decoration.GetComponent<SpriteRenderer>().flipX = true;
+				// move slightly to the right if it is on the left side depending on the size of the decoration
+				decoration.transform.position += new Vector3(decoration.GetComponent<SpriteRenderer>().bounds.size.x / 2, 0, 0);
+			}
+			else {
+				// move slightly to the left if it is on the right side
+				decoration.transform.position += new Vector3(-decoration.GetComponent<SpriteRenderer>().bounds.size.x / 2, 0, 0);
+			}
+			
 		}
 	}
 }
